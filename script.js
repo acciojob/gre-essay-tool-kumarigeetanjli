48c30a6f3df743094b73922338d7b07747b72b83
kumarigeetanjli <84951879+kumarigeetanjli@users.noreply.github.com>
2023-01-01 17:12:42 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 //your code here
+const textBox = document.getElementById('evaluatedText');
+const wordCount = document.getElementById('wordCount');
+
+// Add an event listener to the textbox that listens for changes
+textBox.addEventListener('keyup', function() {
+    
+    if(textBox.value === ""){
+        wordCount.innerText = 0
+    }
+    else{
+        const text = textBox.value.trim().split(" ")
+
+        if(text.length > 0)
+          {
+            wordCount.innerText = text.length
+          }
+    }
+ 
+});
